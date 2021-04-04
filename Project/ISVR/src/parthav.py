7a608409644268252dfccfc55d554a11ff9fe393
ParthavK <80574271+ParthavK@users.noreply.github.com>
2021-04-04 15:43:33 +0530

    Add files via upload

---
+++ Project/ISVR/src/parthav.py
@@ -0,0 +1,18 @@
+'''
+Name: Parthav Kansagra
+  ID: 201801145
+
+'''
+import webbrowser
+import os
+def webex():
+    webbrowser.open("https://www.webex.com")
+
+def webex_caller():
+    webex
+
+hash_dict={
+    "open webex": webex_caller,
+    "open webex meeting": webex_caller
+}
+# i will do..
