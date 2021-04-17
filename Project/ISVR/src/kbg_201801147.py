bc332c98b09f8144dc364611f44480ba6b0992db
kbg1910 <72680317+kbg1910@users.noreply.github.com>
2021-04-17 22:25:06 +0530

    Open text Editor
    i) open notepad in windows through voice command.
    ii) open Gedit in Linux through voice command.
    iii) open pages in mac through voice command.

---
+++ Project/ISVR/src/kbg_201801147.py
@@ -0,0 +1,38 @@
+import os
+import platform
+from src.va import speak,ask
+
+
+def openTextEditor():
+    pt = platform.system()
+    pt = pt.lower()
+    if pt == 'win32' or pt == 'windows':
+        os.system('notepad')
+    elif pt == 'linux' or pt == 'linux2':
+        os.system('gedit')
+    elif pt == 'darwin':
+        os.system('open /Applications/Pages.app')
+
+
+def open_caller(line):
+    openTextEditor()
+
+
+hash_dict = {
+    "open notepad": open_caller,
+    "start notepad": open_caller,
+    "open text editor": open_caller,
+    "start text editor": open_caller,
+    "open gedit": open_caller,
+    "start gedit": open_caller,
+    "notepad": open_caller,
+    "notepad start": open_caller,
+    "notepad open": open_caller,
+    "text editor": open_caller,
+    "gedit open": open_caller,
+    "gedit start": open_caller,
+    "gedit": open_caller,
+    "text editor start": open_caller,
+    "text editor open": open_caller
+}
+#open_caller("open")
