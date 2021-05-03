ed9c00f06167382eba5986cd623fea2f080e4d2a
kbg1910 <kbgdelta1910@gmail.com>
2021-05-03 11:42:44 +0530

    Update kbg_201801147.py
    1)  Open Notepad in windows using voice command.
    2) Open Gedit in Linux using voice command.
    3) Open pages in Mac using voice command.
    4) merge pdfs into one pdf over voice command.

--- Project/ISVR/src/kbg_201801147.py
+++ Project/ISVR/src/kbg_201801147.py
@@ -1,7 +1,77 @@
 import os
 import platform
-from src.va import speak,ask
-#hello
+from src.va import speak, ask
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+import PyPDF2
+from pathlib import Path
+
+
+def merger_call():
+    merger = PyPDF2.PdfFileMerger()
+    list1 = []
+
+    def open_file(files):
+        filepath = askopenfilename(filetypes=[("PDF Files", "*.pdf"), ("All Files", "*.*")])
+        if not (filepath and Path(filepath).exists()):
+            return
+        files.append(filepath)
+        # list out all filenames
+        lbl_items["text"] = '\n'.join(str(f) for f in files)
+        if len(files) >= 2 and btn_merge['state'] == "disabled":
+            btn_merge["state"] = "normal"
+
+    def merge_pdfs(files):
+        for f in files:
+            merger.append(PyPDF2.PdfFileReader(open(f, "rb")))
+
+        output_filename = ent_output_name.get()
+
+        if not output_filename:
+            output_filename = "Untitled.pdf"
+        elif ".pdf" not in output_filename:
+            output_filename += ".pdf"
+        merger.write(output_filename)
+
+    window = tk.Tk()
+    window.title("PDFMerger By KBG")
+    window.geometry("500x500")
+    # not allowed resizing x y direction
+    window.resizable(0, 0)
+
+    # --- Ask open files ---
+    fr_bg1 = tk.Frame(window, bd=3)
+    lbl_open = tk.Label(fr_bg1, text="Please choose PDFs to join: (2 and above)")
+    lbl_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+
+    btn_open = tk.Button(fr_bg1, text="Open file(s)", command=lambda: open_file(list1))
+    btn_open.grid(row=1, column=0, sticky="ew", padx=5)
+    lbl_items = tk.Label(fr_bg1, text="")
+    lbl_items.grid(row=2, column=0, pady=5)
+    fr_bg1.pack()
+
+    # --- Button to merge PDFs ---
+    fr_bg2 = tk.Frame(window, bd=3)
+    lbl_to_merge = tk.Label(fr_bg2, text="Merge selected files (in PDF)")
+    lbl_to_merge.grid(row=0, column=0, sticky="ew", padx="5", pady="5")
+
+    ent_output_name = tk.Entry(master=fr_bg2, width=7)
+    ent_output_name.grid(row=1, column=0, sticky="ew")
+
+    btn_merge = tk.Button(fr_bg2, text="Merge PDF", state="disabled", command=lambda: merge_pdfs(list1))
+    btn_merge.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+    fr_bg2.pack()
+
+    # --- Button to exit ---
+    btn_exit = tk.Button(window, text="Exit", command=window.destroy, bd=2)
+    btn_exit.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=tk.FALSE)
+
+    window.mainloop()
+
+
+def open_merger(line):
+    merger_call()
+
 
 def openTextEditor():
     pt = platform.system()
@@ -35,4 +105,4 @@ hash_dict = {
     "text editor start": open_caller,
     "text editor open": open_caller
 }
-#open_caller("open")
+# open_merger("open")
